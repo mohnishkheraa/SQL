3ccd89d573a0a716a285b4d5188dc5828d5cabcf
mohnishkheraa <mohnishkhera@ltts.com>
2021-03-08 15:02:49 +0530

    Day 3

---
+++ Mini Project/MiniProject/src/com/ltts/configuration/MyConnection.java
@@ -0,0 +1,17 @@
+package com.ltts.configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MyConnection {
+	
+	public static Connection getConnection() throws Exception
+	{
+		
+		Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/db","root","admin");
+		return c;
+		
+	}
+	
+
+}
